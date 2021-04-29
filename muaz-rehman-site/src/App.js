38d001fb10e752b5bc2b88c73086e28962338234
Muaz Rehman <muaz_r5@Hotmail.com>
2021-04-28 20:54:49 -0400

    first commit

---
+++ muaz-rehman-site/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Work In Progress!
+        </p>
+        <a
+          className="App-link"
+          href="https://www.muaz.ca"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Muaz Rehman
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
